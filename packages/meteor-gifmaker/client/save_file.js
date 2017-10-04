a4a8945298aab6203fbfd6e600732656e6dc6968
Jason Shao <jason@Jasons-MacBook-Pro.local>
2017-10-04 03:23:16 -0700

    downloadable image and some unit testing client side with jasmine

--- packages/meteor-gifmaker/client/save_file.js
+++ packages/meteor-gifmaker/client/save_file.js
@@ -1,13 +1,13 @@
 
 Meteor.saveFile = function (event) {
-    FS.Utility.eachFile(event, function (file) { //allow only one image
+    FS.Utility.eachFile(event, function (file) { //multiple images selectable?
+        console.log('file: ', file)
         var newFile = new FS.File(file);
         Images.insert(newFile, function (error, result) {
             if (error) {
-                console.log("Error uploading the image");
+                console.log("Error uploading image");
             } else {
                 var numOfImages = Meteor.count();
-
                 if (!!event.currentTarget.labels) {
                     event.currentTarget.labels[numOfImages - 1].innerHTML = file.name;
                 }
@@ -21,25 +21,23 @@ Meteor.saveFile = function (event) {
     });
 }
 
-Meteor.count = function () {
+Meteor.count = function () { //how many images are in collection
     return Images.find().count();
 }
 
-Meteor.clearImages = function (event) {
+Meteor.clearImages = function (event) { //remove all images in collection
     Images.find().forEach(function (file) {
         file.remove();
     });
 }
 
 Meteor.handleClear = function (event) {
-    console.log('clear clicked')
     Meteor.clearImages(); // Empties Image Collection
     var displayNode = document.getElementById('displayGIF')
-    while (displayNode.hasChildNodes()) {
+    while (displayNode.hasChildNodes()) { //set displayGIF to empty
         displayNode.removeChild(displayNode.lastChild);
     }
-
-    var $labelImages = document.getElementsByClassName('label-image');
+    var $labelImages = document.getElementsByClassName('label-image'); //reset file name to original
     for (var i = 0; i < $labelImages.length; i++) {
         $labelImages[i].innerHTML = '<strong><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg> Choose a file&hellip;</strong>'
     }
--- packages/meteor-gifmaker/package.js
+++ packages/meteor-gifmaker/package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'meteor-gifmaker',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'images to gif with meteor',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/Selectful/MeteorGifMakerTest/tree/master/packages/meteor-gifmaker',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -22,6 +22,16 @@ Package.onUse(function (api) {
   api.addFiles('client/save_file.js', 'client');
   api.addFiles('client/submit_file.js', 'client');
 });
+
+// Package.onTest(function(api) {
+//   // You almost definitely want to depend on the package itself,
+//   // this is what you are testing!
+//   api.use('meteor-gifmaker');
+//   // You should also include any packages you need to use in the test code
+//   api.use(['practicalmeteor:mocha']);
+//   // Finally add an entry point for tests
+//   api.mainModule('tests/tests.js');
+// });
 
 
 
